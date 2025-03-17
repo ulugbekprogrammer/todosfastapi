3ab120f87794b4694f675ee459b7059c05b3fbd8
Ulugbek <urakovulugbek571@gmail.com>
2025-03-17 13:07:25 -0400

    first commit

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -3,11 +3,18 @@ from sqlalchemy.orm import Session
 from ..database import SessionLocal
 from ..models import Todos
 from starlette import status
-from fastapi import APIRouter, Depends, HTTPException, Path
+from fastapi import APIRouter, Depends, HTTPException, Path, Request, status
 from pydantic import BaseModel, Field
 from .auth import get_current_user
+from starlette.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
 
-router = APIRouter()
+templates = Jinja2Templates(directory="TodoApp/templates")
+
+router = APIRouter(
+    prefix='/todos',
+    tags=['todo']
+)
 
 def get_db():
     db = SessionLocal()
@@ -25,6 +32,52 @@ class TodoRequest(BaseModel):
     priority: int = Field(gt=0, lt=6)
     complete: bool
 
+def redirect_to_login():
+    redirect_response = RedirectResponse(url='/login', status_code=status.HTTP_302_FOUND)
+    redirect_response.delete_cookie(key='access_token')
+    return redirect_response
+
+
+### Pages ###
+
+@router.get('/todo-page')
+async def render_todo_page(request: Request, db: db_dependency):
+    try:
+        user = await get_current_user(request.cookies.get('access_token'))
+
+        if user is None:
+            return redirect_to_login()
+        todos = db.query(Todos).filter(Todos.owner_id == user.get('id')).all()
+
+        return templates.TemplateResponse('todo.html', {'request': request, 'todos': todos, 'user': user})
+    except:
+        return redirect_to_login()
+    
+@router.get('/add-todo-page')
+async def render_todo_page(request: Request):
+    try:
+        user = await get_current_user(request.cookies.get('access_token'))
+
+        if user is None:
+            return redirect_to_login()
+        return templates.TemplateResponse('add-todo.html', {'request': request, 'user': user})
+    except:
+        return redirect_to_login()
+@router.get('/edit-todo-page/{todo_id}')
+async def render_edit_todo_page(request: Request, todo_id: int, db: db_dependency):
+    try: 
+        user = await get_current_user(request.cookies.get('access_token'))
+
+        if user is None:
+            return redirect_to_login()
+        todo = db.query(Todos).filter(Todos.id == todo_id).first()
+
+        return templates.TemplateResponse('edit-todo.html', {'request': request, 'todo': todo, 'user': user})
+    except:
+        return redirect_to_login()
+    
+
+### Endpoints ###
 
 @router.get('/', status_code=status.HTTP_200_OK)
 async def read_all(user: user_dependency, db: db_dependency):
--- TodoApp/test/test_todos.py
+++ TodoApp/test/test_todos.py
@@ -8,7 +8,7 @@ app.dependency_overrides[get_db] = override_get_db
 app.dependency_overrides[get_current_user] = override_get_current_user
 
 def test_read_all_authenticated(test_todo):
-    response = client.get('/')
+    response = client.get('/todos')
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == [{
         'complete': False,
@@ -20,7 +20,7 @@ def test_read_all_authenticated(test_todo):
     }]
 
 def test_read_one_authentication(test_todo):
-    response = client.get('/todo/1')
+    response = client.get('/todos/todo/1')
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {
         'complete': False,
@@ -32,7 +32,7 @@ def test_read_one_authentication(test_todo):
     }
 
 def test_read_one_authentication_not_found():
-    response = client.get('/todo/999')
+    response = client.get('/todos/todo/999')
     assert response.status_code == 404
     assert response.json() == {'detail': 'Todo not found.'}
 
@@ -44,7 +44,7 @@ def test_create_todo(test_todo):
         'complete': False
     }
 
-    response = client.post('/todo/', json=request_data)
+    response = client.post('/todos/todo/', json=request_data)
     assert response.status_code == 201
 
     db = TestingSessionLocal()
@@ -62,7 +62,7 @@ def test_update_todo(test_todo):
         'complete': False
     }
 
-    response = client.put('/todo/1', json=request_data)
+    response = client.put('/todos/todo/1', json=request_data)
     assert response.status_code == 204
     db = TestingSessionLocal()
     model = db.query(Todos).filter(Todos.id == 1).first()
@@ -76,18 +76,18 @@ def test_update_todo_not_found(test_todo):
         'complete': False
     }
 
-    response = client.put('/todo/999', json=request_data)
+    response = client.put('/todos/todo/999', json=request_data)
     assert response.status_code == 404
     assert response.json() == {'detail': 'Todo not found.'}
 
 def test_delete_todo(test_todo):
-    response = client.delete('/todo/1')
+    response = client.delete('/todos/todo/1')
     assert response.status_code == 204
     db = TestingSessionLocal()
     model = db.query(Todos).filter(Todos.id == 1).first()
     assert model is None
 
 def test_delete_todo_not_found():
-    response = client.delete('/todo/999')
+    response = client.delete('/todos/todo/999')
     assert response.status_code == 404
     assert response.json() == {'detail': 'Todo not found.'}
